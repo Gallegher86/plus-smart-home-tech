e5d765bf357cac7799b2a8b8d0d47e7be84f7c72
Anton Riabushkin <riabushkinanton@yandex.ru>
2026-04-05 18:20:26 +0700

    feat: контроллер, сервис.

--- telemetry/collector/src/main/java/ru/yandex/practicum/CollectorApplication.java
+++ telemetry/collector/src/main/java/ru/yandex/practicum/CollectorApplication.java
@@ -2,8 +2,11 @@ package ru.yandex.practicum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import ru.yandex.practicum.kafka.KafkaProducerConfiguration;
 
 @SpringBootApplication
+@EnableConfigurationProperties(KafkaProducerConfiguration.class)
 public class CollectorApplication {
     public static void main(String[] args) {
         SpringApplication.run(CollectorApplication.class, args);
---
+++ telemetry/collector/src/main/java/ru/yandex/practicum/service/CollectorServiceImpl.java
@@ -0,0 +1,82 @@
+package ru.yandex.practicum.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.avro.specific.SpecificRecordBase;
+import org.springframework.stereotype.Service;
+import ru.yandex.practicum.dto.hubs.*;
+import ru.yandex.practicum.dto.sensors.*;
+import ru.yandex.practicum.kafka.KafkaCollectorProducer;
+import ru.yandex.practicum.kafka.KafkaTopic;
+import ru.yandex.practicum.mapper.HubEventMapper;
+import ru.yandex.practicum.mapper.SensorEventMapper;
+import ru.yandex.practicum.kafka.telemetry.event.HubEventAvro;
+import ru.yandex.practicum.kafka.telemetry.event.SensorEventAvro;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class CollectorServiceImpl implements CollectorService {
+    private final SensorEventMapper sensorEventMapper;
+    private final HubEventMapper hubEventMapper;
+    private final KafkaCollectorProducer producer;
+
+    @Override
+    public void processSensorEvent(SensorEvent event) {
+        log.info("Processing sensor event: id={}, type={}", event.getId(), event.getType());
+        SpecificRecordBase eventAvro = mapToSensorEventAvro(event);
+        log.debug("Sensor event Avro payload: {}", eventAvro);
+        producer.send(KafkaTopic.SENSOR, event.getId(), eventAvro);
+    }
+
+    @Override
+    public void processHubEvent(HubEvent event) {
+        log.info("Processing hub event: hubId={}, type={}", event.getHubId(), event.getType());
+        SpecificRecordBase eventAvro = mapToHubEventAvro(event);
+        log.debug("Hub event Avro payload: {}", eventAvro);
+        producer.send(KafkaTopic.HUB, event.getHubId(), eventAvro);
+    }
+
+    private SpecificRecordBase mapToSensorEventAvro(SensorEvent event) {
+        SpecificRecordBase payload = switch (event.getType()) {
+            case CLIMATE_SENSOR_EVENT -> sensorEventMapper
+                    .toClimateSensorEventAvro((ClimateSensorEvent) event);
+            case LIGHT_SENSOR_EVENT -> sensorEventMapper
+                    .toLightSensorEventAvro((LightSensorEvent) event);
+            case MOTION_SENSOR_EVENT -> sensorEventMapper
+                    .toMotionSensorEventAvro((MotionSensorEvent) event);
+            case SWITCH_SENSOR_EVENT -> sensorEventMapper
+                    .toSwitchSensorEventAvro((SwitchSensorEvent) event);
+            case TEMPERATURE_SENSOR_EVENT -> sensorEventMapper
+                    .toTemperatureSensorEventAvro((TemperatureSensorEvent) event);
+        };
+
+        return SensorEventAvro.newBuilder()
+                .setId(event.getId())
+                .setHubId(event.getHubId())
+                .setTimestamp(event.getTimestamp())
+                .setType(sensorEventMapper.toSensorEventTypeAvro(event.getType()))
+                .setPayload(payload)
+                .build();
+    }
+
+    private SpecificRecordBase mapToHubEventAvro(HubEvent event) {
+        SpecificRecordBase payload = switch (event.getType()) {
+            case DEVICE_ADDED -> hubEventMapper
+                    .toDeviceAddedEventAvro((DeviceAddedEvent) event);
+            case DEVICE_REMOVED -> hubEventMapper
+                    .toDeviceRemovedEventAvro((DeviceRemovedEvent) event);
+            case SCENARIO_ADDED -> hubEventMapper
+                    .toScenarioAddedEventAvro((ScenarioAddedEvent) event);
+            case SCENARIO_REMOVED -> hubEventMapper
+                    .toScenarioRemovedEventAvro((ScenarioRemovedEvent) event);
+        };
+
+        return HubEventAvro.newBuilder()
+                .setHubId(event.getHubId())
+                .setTimestamp(event.getTimestamp())
+                .setType(hubEventMapper.toHubEventTypeAvro(event.getType()))
+                .setPayload(payload)
+                .build();
+    }
+}
